c2afbc03b81286751f782898c527fb9b4c0dc8f2
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2021-03-09 12:09:35 +0100

    Returned value as json object

--- masterfile.py
+++ masterfile.py
@@ -1,17 +1,25 @@
-MASTER_FILE = open("CAPmast.txt", "r")
+import sys
+import json
 
-master_dump_array = []
-for line in MASTER_FILE:
-  master_dump_array.append({
-    "CHN": line[0:15].strip(), 
-    'fullname': line[15:45].strip(),
-    'address': line[55:120].strip(),
-    'location': line[120:200].strip(),
-    'bank': line[200:243].strip(),
-    'rand_num': line[243:273].strip(),
-    'email': line[273:313].strip(),
-    'rand_num_2': line[313:353].strip(),
-    "contact": line[353:380].strip()
-  })
+# MASTER_FILE = open("CAPmast.txt", "r")
+MASTER_FILE = sys.argv
 
-print(master_dump_array)
+def dataParser():
+  master_dump_array = []
+  for line in MASTER_FILE:
+    master_dump_array.append({
+      "CHN": line[0:15].strip(), 
+      'fullname': line[15:45].strip(),
+      'address': line[55:120].strip(),
+      'location': line[120:200].strip(),
+      'bank': line[200:243].strip(),
+      'rand_num': line[243:273].strip(),
+      'email': line[273:313].strip(),
+      'rand_num_2': line[313:353].strip(),
+      "contact": line[353:380].strip()
+    })
+
+  return json.dumps(master_dump_array)
+
+
+dataParser()
--- transaction.py
+++ transaction.py
@@ -1,22 +1,30 @@
 import re
+import sys
+import json
 
-TRANSACTION_FILE = open("CAPs6.txt", "r")
+# TRANSACTION_FILE = open("CAPs6.txt", "r")
+TRANSACTION_FILE = sys.argv
 
-transaction_dump_array = []
-for line in TRANSACTION_FILE:
-  columns = line.split() # Split columns using space as delimiter
-  date_chars_arr = re.findall('[0-9]', columns[2]) # Get the date from column three
-  company_code = re.findall('[A-Z]', columns[2]) # Get the company code from column three
-  symbol_split = re.split('[A-Z]', columns[4])
-  transaction_dump_array.append({
-    'column_one': columns[0],
-    'column_two': columns[1],
-    'date': ''.join(date_chars_arr),
-    'company_code': ''.join(company_code),
-    'column_five': columns[3],
-    'column_six': re.split('[-+]', columns[4])[0],
-    'type': re.split('[0]', symbol_split[0])[1], # Transaction type
-    'column_eight': re.split('[-+]', columns[4])[1]
-  })
-  
-print(transaction_dump_array)
+def dataParser():
+  transaction_dump_array = []
+  for line in TRANSACTION_FILE:
+      columns = line.split()  # Split columns using space as delimiter
+      # Get the date from column three
+      date_chars_arr = re.findall('[0-9]', columns[2])
+      # Get the company code from column three
+      company_code = re.findall('[A-Z]', columns[2])
+      symbol_split = re.split('[A-Z]', columns[4])
+      transaction_dump_array.append({
+          'column_one': columns[0],
+          'column_two': columns[1],
+          'date': ''.join(date_chars_arr),
+          'company_code': ''.join(company_code),
+          'column_five': columns[3],
+          'column_six': re.split('[-+]', columns[4])[0],
+          'type': re.split('[0]', symbol_split[0])[1],  # Transaction type
+          'column_eight': re.split('[-+]', columns[4])[1]
+      })
+
+  return json.dumps(transaction_dump_array)
+
+dataParser()
